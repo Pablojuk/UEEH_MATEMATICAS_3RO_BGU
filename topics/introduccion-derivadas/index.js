b63d5eac377e3bfbf6a2e699b8a1e39419e8fa18
Pablojuk <pablojuk@hotmail.com>
2026-05-17 16:31:12 -0500

    Agregar estructura base de Introducción a las Derivadas

---
+++ topics/introduccion-derivadas/index.js
@@ -0,0 +1,4 @@
+export { topicConfig } from "./topic-config.js";
+export { slides } from "./slides.js";
+export { gameData } from "./game-data.js";
+export { homeworkData } from "./homework-data.js";
